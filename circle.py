c0b84953e92b59fcadd07073028de90b3cc50934
KarenWaskewitz <karen.waskewitz@gmail.com>
2025-04-07 11:20:12 +0300

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,6 @@
+import math
+r = float(input("Enter the radius: "))
+circumference=2*r*math.pi
+area=math.pi*r*r
+print("The circumference is: ", circumference)
+print("The area is: ", area)
---
+++ rectangle.py
@@ -0,0 +1,6 @@
+height = int(input("Enter height: "))
+width = int(input("Enter width: "))
+perimeter = 2*(height + width)
+area = height*width
+print("This is the area: ", area)
+print("This is the perimeter: ", perimeter)
